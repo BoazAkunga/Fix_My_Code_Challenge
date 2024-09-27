ca5c4e530133e7f21491adec6a54a7cc71f27e76
BoazAkunga <boazakunga@gmail.com>
2024-09-27 07:39:29 -0700

    sort

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -15,15 +15,14 @@ ARGV.each do |arg|
     # insert result at the right position
     is_inserted = false
     i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
+    while i < result.size do
+        if result[i] >= i_arg
+          result.insert(i, i_arg)
+          is_inserted = true
             i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
             break
         end
+        i += 1
     end
     result << i_arg if !is_inserted
 end
